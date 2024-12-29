a9c6a76d7c6e90f1af5b285b8e8b10f48073059e
UnemployedProgrammer <sebastianmuellerreallife@gmail.com>
2024-12-29 12:03:24 +0100

    My pc crashed recently, making many commits for a week! Datagen for Shadow Stairs, etc. done!

--- src/main/java/com/sebastian/levoria/network/ShakeScreenS2C.java
+++ src/main/java/com/sebastian/levoria/network/ShakeScreenS2C.java
@@ -4,14 +4,21 @@ import com.sebastian.levoria.Levoria;
 import net.minecraft.block.Block;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
-public record ShakeScreenS2C(int duration) implements CustomPayload {
+public record ShakeScreenS2C(int duration, float strength) implements CustomPayload {
+    public static final Id<ShakeScreenS2C> ID = new Id<>(Levoria.getId("shake_screen"));
+    public static final PacketCodec<RegistryByteBuf, ShakeScreenS2C> PACKET_CODEC =
+            PacketCodec.tuple(
+                    PacketCodecs.INTEGER, ShakeScreenS2C::duration,
+                    PacketCodecs.FLOAT, ShakeScreenS2C::strength,
+                    ShakeScreenS2C::new);
 
     @Override
     public Id<? extends CustomPayload> getId() {
-        return null;
+        return ID;
     }
 }
