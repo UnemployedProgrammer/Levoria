4fe9c05d83ad6aa988828868f9a159f3d170f1b2
UnemployedProgrammer <sebastianmuellerreallife@gmail.com>
2025-01-06 20:18:52 +0100

    Moon World Placer (Dev Util)

---
+++ src/main/java/com/sebastian/levoria/debug/MoonWorldPiecePlacer.java
@@ -0,0 +1,81 @@
+package com.sebastian.levoria.debug;
+
+import com.sebastian.levoria.Levoria;
+import com.sebastian.levoria.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.UUID;
+
+public class MoonWorldPiecePlacer {
+    private final World world;
+    private final BlockPos bottomLeftCorner;
+    private final int sizeX, sizeY, sizeZ;
+
+    public MoonWorldPiecePlacer(World world, BlockPos bottomLeftCorner, int sizeX, int sizeY, int sizeZ) {
+        this.world = world;
+        this.bottomLeftCorner = bottomLeftCorner;
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.sizeZ = sizeZ;
+    }
+
+    private void placeBlock(Block block, BlockPos pos) {
+        world.getServer().execute(() -> {
+            world.setBlockState(pos, block.getDefaultState());
+        });
+    }
+
+    private void placeMoonStone(BlockPos pos) {
+        world.getServer().execute(() -> {
+            world.setBlockState(pos, ModBlocks.MOON_STONE.getDefaultState(), Block.NOTIFY_LISTENERS);
+        });
+    }
+
+    public void generateWorld() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int x = 0; x < sizeX ; x++) {
+                        for (int y = 0; y < sizeY ; y++) {
+                            for (int z = 0; z < sizeZ ; z++) {
+                                placeMoonStone(bottomLeftCorner.add(x, y, z));
+                                Thread.sleep(1);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    Levoria.LOGGER.warn("Error while generating...");
+                    Levoria.LOGGER.warn(e.toString());
+                }
+            }
+        }, "placeMoonStructureThread");
+        t.start();
+    }
+
+    public void clearWorld() {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int x = 0; x < sizeX ; x++) {
+                        for (int y = 0; y < sizeY ; y++) {
+                            for (int z = 0; z < sizeZ ; z++) {
+                                placeBlock(Blocks.AIR, bottomLeftCorner.add(x, y, z));
+                                Thread.sleep(1);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    Levoria.LOGGER.warn("Error while clearing...");
+                    Levoria.LOGGER.warn(e.toString());
+                }
+            }
+        }, "clearMoonStructureThread#" + UUID.randomUUID().toString());
+        t.start();
+    }
+}
--- src/main/java/com/sebastian/levoria/util/Commands.java
+++ src/main/java/com/sebastian/levoria/util/Commands.java
@@ -15,6 +15,7 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.sebastian.levoria.config.ConfigEditor;
 import com.sebastian.levoria.config.ConfigManager;
+import com.sebastian.levoria.debug.MoonWorldPiecePlacer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
@@ -24,11 +25,15 @@ import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Commands {
+
+    public static HashMap<String, MoonWorldPiecePlacer> placers = new HashMap<>();
+
         public static void registerCommands() {
             CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
                 dispatcher.register(literal("shakescreen").requires((source) -> source.hasPermissionLevel(2))
@@ -62,11 +67,11 @@ public class Commands {
                                                                 }))))));
 
                 dispatcher.register(literal("levoria").requires((source) -> source.hasPermissionLevel(4))
-                        .then(CommandManager.literal("configure")
+                        .then(literal("configure")
                                 .then(argument("changes_or_leave_empty_for_help", StringArgumentType.string())
 
                                         .executes(context -> {
-                                            final String changes = StringArgumentType.getString(context, "changes_or_leave_empty_for_help");
+                                            final String changes = getString(context, "changes_or_leave_empty_for_help");
 
                                             if(changes.isBlank()) {
                                                 context.getSource().sendMessage(Text.literal("-- Help --").formatted(Formatting.GREEN));
@@ -92,12 +97,70 @@ public class Commands {
                                             return 1;
                                         })))
 
-                                .then(CommandManager.literal("fetch_cfg")
+                                .then(literal("fetch_cfg")
 
                                         .executes(context -> {
                                             context.getSource().sendMessage(Text.literal(ConfigManager.INSTANCE.toString()).setStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Click to modify..."))).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/levoria configure \"\"")).withClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/levoria configure \"\""))).formatted(Formatting.GRAY));
                                             return 1;
                                         }))
+
+                                .then(literal("generate_moon_world")
+                                        .then(argument("name", StringArgumentType.string())
+                                        .then(argument("sizeX", IntegerArgumentType.integer(1, 200))
+                                        .then(argument("sizeY", IntegerArgumentType.integer(1, 200))
+                                        .then(argument("sizeZ", IntegerArgumentType.integer(1, 200))
+
+                                        .executes(context -> {
+                                            try {
+                                                final String name = StringArgumentType.getString(context, "name");
+                                                final int sizeX = IntegerArgumentType.getInteger(context, "sizeX");
+                                                final int sizeY = IntegerArgumentType.getInteger(context, "sizeY");
+                                                final int sizeZ = IntegerArgumentType.getInteger(context, "sizeZ");
+                                                final Vec3d pos = context.getSource().getPosition();
+                                                final BlockPos bPos = new BlockPos((int) Math.round(pos.x), (int) Math.round(pos.y), (int) Math.round(pos.z));
+
+                                                MoonWorldPiecePlacer placer = new MoonWorldPiecePlacer(context.getSource().getWorld(), bPos, sizeX, sizeY, sizeZ);
+                                                placers.put(name, placer);
+                                                placer.generateWorld();
+                                                context.getSource().sendMessage(Text.literal("Generated at" + bPos.toShortString()));
+                                            } catch (Exception e) {
+                                                context.getSource().sendError(Text.literal("Error while placing: " + e.getMessage()));
+                                            }
+                                            return 1;
+                                        }))))))
+                                .then(CommandManager.literal("clear_wld")
+                                        .then(CommandManager.literal("single")
+                                                        .then(argument("name", StringArgumentType.string())
+                                                                        .executes(context -> {
+                                                                            try {
+                                                                                final String name = StringArgumentType.getString(context, "name");
+
+                                                                                MoonWorldPiecePlacer placer = placers.get(name);
+                                                                                placer.clearWorld();
+                                                                                placers.remove(name);
+                                                                                context.getSource().sendMessage(Text.literal("Cleared " + name));
+                                                                            } catch (Exception e) {
+                                                                                context.getSource().sendError(Text.literal("Error while clearing: " + e.getMessage()));
+                                                                            }
+                                                                            return 1;
+                                                                        })))
+                                        .then(CommandManager.literal("all")
+                                                .executes(context -> {
+                                                    Iterator<Map.Entry<String, MoonWorldPiecePlacer>> iterator = placers.entrySet().iterator();
+                                                    while (iterator.hasNext()) {
+                                                        Map.Entry<String, MoonWorldPiecePlacer> entry = iterator.next();
+                                                        try {
+                                                            MoonWorldPiecePlacer placer = entry.getValue();
+                                                            placer.clearWorld();
+                                                            iterator.remove(); // Safely remove the entry
+                                                            context.getSource().sendMessage(Text.literal("Cleared " + entry.getKey()));
+                                                        } catch (Exception e) {
+                                                            context.getSource().sendError(Text.literal("Error while clearing: " + e.getMessage()));
+                                                        }
+                                                    }
+                                                    return 1;
+                                                })
+                                        ))
 
                         /*
                         .then(CommandManager.literal("add")
